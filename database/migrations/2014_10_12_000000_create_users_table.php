6f62b993e15586099a00812e9e16ffb2ea0851bf
myrealjay <nwan.james@okbcoy.com.ng>
2019-03-12 09:55:54 +0100

    added platform to users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('authorization_token')->nullable();
+            $table->string('platform');
             $table->rememberToken();
             $table->timestamps();
         });
